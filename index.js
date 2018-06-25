1cd53a70ed06fe612a4558555becf4d5641da07e
zhangshuai <hzzs@hzdomain1.com>
2018-06-25 16:41:17 +0800

    add koa-onerror

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const config = require('./config');
-const {logger} = require('./utils');
+const { logger, helper: {fail} } = require('./utils');
 const routes = require('./routes');
+const onerror = require('koa-onerror');
 
 const app = new Koa();
+
+onerror(app, {
+  all: ctx => {
+    ctx.body = fail(500, 'internal error');
+  }
+});
 
 app.use(bodyParser());
 app.use(routes.routes(), routes.allowedMethods());
